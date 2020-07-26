c0e780009c1e530373952b51c10e6a2b435f1630
Big W <wendyuworks@gmail.com>
2020-07-26 23:28:22 +1000

    install font-awesome |  add arrow to browse button

--- src/pages/components/BrowseButton.js
+++ src/pages/components/BrowseButton.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
-import 'bulma'
+import 'bulma';
+import 'font-awesome/css/font-awesome.min.css';
 
 class BrowseButton extends Component{
     render(){
         return(
             <div>
-                <div class="button is-info">
-                    Browse
+                <div class="button is-info is-rounded is-medium">
+                    <div>Browse events   </div>
+                    <div class="has-text-info">-</div>
+                    <i className="fa fa-arrow-right" aria-hidden="true"/>
                 </div>
             </div>
         )
