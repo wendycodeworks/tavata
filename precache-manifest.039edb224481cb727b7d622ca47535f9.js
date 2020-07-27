d616d3fb1281a7d73a47427d8821c6d990b0a257
Big W <wendyuworks@gmail.com>
2020-07-27 11:28:49 +1000

    Updates

---
+++ precache-manifest.039edb224481cb727b7d622ca47535f9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7277a08d2a94d62fcf94025e3e89b35c",
+    "url": "/tavata/index.html"
+  },
+  {
+    "revision": "7aaaa17447f3b90a959a",
+    "url": "/tavata/static/css/2.48509d29.chunk.css"
+  },
+  {
+    "revision": "7aaaa17447f3b90a959a",
+    "url": "/tavata/static/js/2.54de500e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tavata/static/js/2.54de500e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b40bb670deb870ae8fb4",
+    "url": "/tavata/static/js/main.75e27355.chunk.js"
+  },
+  {
+    "revision": "19cc80e95a2430420feb",
+    "url": "/tavata/static/js/runtime-main.2feb6aa3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/tavata/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/tavata/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/tavata/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/tavata/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/tavata/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "dbeac699974352e4ce57f2ba5eb887b1",
+    "url": "/tavata/static/media/logo.dbeac699.png"
+  }
+]);
